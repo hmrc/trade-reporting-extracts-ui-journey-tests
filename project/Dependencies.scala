3d69c4cd793ca21c6b491e72acfb98a2af6cbc42
shubhay2018 <171135311+Shubhay2018@users.noreply.github.com>
2025-07-02 16:21:37 +0100

    TRE-661: Upadated UI Test Runner to latest

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val test: Seq[ModuleID] = Seq(
     "com.vladsch.flexmark" % "flexmark-all"   % "0.64.8" % Test,
     "org.scalatest"       %% "scalatest"      % "3.2.17" % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner" % "0.45.0" % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner" % "0.46.0" % Test,
     "org.pegdown"          % "pegdown"        % "1.6.0"  % Test,
     "com.typesafe"         % "config"         % "1.4.3"  % Test
   )
