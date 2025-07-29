25d673c6e0bf48a5d2457851afc8390a67993de4
Alex-H-94 <23293049+Alex-H-94@users.noreply.github.com>
2025-07-29 14:44:57 +0100

    Update BasePage.scala
    Cleaning up by removing redundant / unused tests and adjusting variable names.

--- src/test/scala/uk/gov/hmrc/ui/pages/BasePage.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/BasePage.scala
@@ -17,6 +17,12 @@
 package uk.gov.hmrc.ui.pages
 
 import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.support.ui.{FluentWait, Wait}
+import org.openqa.selenium.support.ui.ExpectedConditions
+
+import java.time.Duration
+
 import uk.gov.hmrc.configuration.TestEnvironment
 import uk.gov.hmrc.selenium.component.PageObject
 import uk.gov.hmrc.selenium.webdriver.Driver
@@ -24,10 +30,14 @@ import uk.gov.hmrc.selenium.webdriver.Driver
 abstract class BasePage(relativeUrl: String, relativeTitle: String) extends PageObject {
 
   protected val baseUrl: String = TestEnvironment.url("trade-reporting-extracts-frontend")
-  val url: String               = baseUrl + relativeUrl
+  val pageUrl: String           = baseUrl + relativeUrl
   val pageTitle: String         = s"$relativeTitle - Get customs declaration data for imports and exports - GOV.UK"
 
-  def navigateTo(urlToGet: String = url): Unit =
+  def fluentWait: Wait[WebDriver] = new FluentWait[WebDriver](Driver.instance)
+    .withTimeout(Duration.ofSeconds(5))
+    .pollingEvery(Duration.ofSeconds(2))
+
+  def navigateTo(urlToGet: String = pageUrl): Unit =
     get(urlToGet)
 
   def continue(): Unit =
@@ -49,27 +59,13 @@ abstract class BasePage(relativeUrl: String, relativeTitle: String) extends Page
     if (index == 1) click(By.cssSelector(s"#value-no"))
   }
 
-  // NOTE: This will only work if you return to the page. Selecting a checkbox/radio option doesn't update the HTML immediately.
-  def assertOptionSelected(index: Int): Unit =
-    assert(
-      Driver.instance.findElement(By.cssSelector(s"#value_$index[checked]")).isSelected(),
-      s"Option $index is not marked as 'checked' (is not selected)."
-    )
-
-  def assertOptionText(index: Int, textToCheck: String): Unit = {
-    val optionText: String = getText(By.cssSelector(s"label.govuk-label[for=value_$index]"))
-    assert(
-      optionText.contains(textToCheck),
-      s"Option $index text was [$optionText], but expected it to contain [$textToCheck]."
-    )
-  }
-
   def assertPageTitle(titleToCheck: String = pageTitle): Unit =
     assert(
       getTitle.contains(titleToCheck),
       s"Page title was [$getTitle], but it was expected to contain '[$titleToCheck]'."
     )
 
-  def assertUrl(urlToCheck: String = url): Unit =
-    assert(getCurrentUrl == urlToCheck, s"Url was: [$getCurrentUrl], but [$url] was expected.")
+  def assertUrl(urlToCheck: String = pageUrl): Unit =
+    fluentWait.until(ExpectedConditions.urlContains(urlToCheck))
+    assert(getCurrentUrl == urlToCheck, s"Url was: [$getCurrentUrl], but [$urlToCheck] was expected.")
 }
