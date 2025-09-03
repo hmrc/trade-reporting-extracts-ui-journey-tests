9e3dffcf6ba1f3ec78c1dc5945a4c36d9e16bce5
shubhay2018 <171135311+Shubhay2018@users.noreply.github.com>
2025-09-03 13:42:06 +0100

    Updated Test Runner to latest

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,8 +5,8 @@ object Dependencies {
   val test: Seq[ModuleID] = Seq(
     "com.vladsch.flexmark" % "flexmark-all"   % "0.64.8" % Test,
     "org.scalatest"       %% "scalatest"      % "3.2.19" % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner" % "0.48.0" % Test,
-    "com.typesafe"         % "config"         % "1.4.3"  % Test
+    "uk.gov.hmrc"         %% "ui-test-runner" % "0.49.0" % Test,
+    "com.typesafe"         % "config"         % "1.4.4"  % Test
   )
 
 }
