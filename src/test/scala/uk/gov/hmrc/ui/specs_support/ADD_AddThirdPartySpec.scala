bf47fbb8bdc75d38a627648348218606f6eb261e
Alex-H-94 <23293049+Alex-H-94@users.noreply.github.com>
2025-10-10 15:09:17 +0100

    Update ADD spec
    Changing the "ADD" designations to "Steps" for simplicity.

--- src/test/scala/uk/gov/hmrc/ui/specs_support/ADD_AddThirdPartySpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs_support/ADD_AddThirdPartySpec.scala
@@ -65,7 +65,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       addThirdPartyPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-1: The user selects 'Continue' to begin the journey.") {
+    Scenario("[F1] Step-1: The user selects 'Continue' to begin the journey.") {
       When("the user clicks to continue")
       addThirdPartyPage.continue()
 
@@ -74,7 +74,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       importerOrExporterPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-2: The user selects 'No' to reach a kickout page.") {
+    Scenario("[F1] Step-2: The user selects 'No' to reach a kickout page.") {
       Given("the user selects the 'No' option")
       importerOrExporterPage.selectOptionByValue("false")
 
@@ -86,7 +86,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       cannotAddThirdPartyPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-2-KO: The user selects the link to return to Dashboard.") {
+    Scenario("[F1] Step-3: The user selects the link to return to Dashboard.") {
       When("the user clicks 'Go to Homepage'")
       dashboardPage.clickLinkToPage()
 
@@ -95,7 +95,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       dashboardPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-2: The user goes back to select 'Yes' to continue instead.") {
+    Scenario("[F1] Step-4: The user goes back to select 'Yes' to continue instead.") {
       Given("the user returns to the 'Importer or Exporter' page")
       importerOrExporterPage.navigateTo()
 
@@ -110,7 +110,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       eoriNumberPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-3: The user enters the EORI of the third party.") {
+    Scenario("[F1] Step-5: The user enters the EORI of the third party.") {
       Given(s"the user enters '$thirdPartyEORI' as the third party EORI")
       eoriNumberPage.clearAndInputKeys(thirdPartyEORI)
 
@@ -122,7 +122,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       confirmEORIPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-4: The user selects 'No' and is taken back to the previous page.") {
+    Scenario("[F1] Step-6: The user selects 'No' and is taken back to the previous page.") {
       Given("the user selects 'No' to enter a different EORI number")
       confirmEORIPage.selectOptionByIndex(1)
 
@@ -134,7 +134,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       eoriNumberPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-4: The user goes back to select 'Yes' to continue instead.") {
+    Scenario("[F1] Step-7: The user goes back to select 'Yes' to continue instead.") {
       Given("the user returns to the 'confirm EORI' page")
       confirmEORIPage.navigateTo()
 
@@ -149,7 +149,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       accessStartPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-6: The user enters when they want access to start.") {
+    Scenario("[F1] Step-8: The user enters when they want access to start.") {
       Given("the user enters a date four years before the current day")
       accessStartPage.clearAndInputKeys(
         accessStartPage.getDateMinusYears("dd", 0),
@@ -172,7 +172,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       accessEndPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-7: The user enters when they want access to end.") {
+    Scenario("[F1] Step-9: The user enters when they want access to end.") {
       Given("the user just clicks continue.")
       accessEndPage.continue()
 
@@ -181,7 +181,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       dataTypesPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-8: The user selects what data types the third party can access.") {
+    Scenario("[F1] Step-10: The user selects what data types the third party can access.") {
       Given("the user selects both 'Import' and 'Export' data types")
       dataTypesPage.selectOptionByIndex(0)
       dataTypesPage.selectOptionByIndex(1)
@@ -194,7 +194,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       giveAccessToDataPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-9: The user selects whether they want to give access to all available data") {
+    Scenario("[F1] Step-11: The user selects whether they want to give access to all available data") {
       Given("the user selects 'no' to set a custom date")
       giveAccessToDataPage.selectOptionByIndex(1)
 
@@ -206,7 +206,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       dataStartPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-10: The user enters the access start date for their data") {
+    Scenario("[F1] Step-12: The user enters the access start date for their data") {
       Given("the user enters a date up to four years from the current date")
       dataStartPage.clearAndInputKeys(
         dataStartPage.getDateMinusYears("dd", 4),
@@ -229,7 +229,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       dataEndPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-11: The user enters the access end date for their data") {
+    Scenario("[F1] Step-13: The user enters the access end date for their data") {
       Given("the user just clicks continue.")
       dataEndPage.continue()
 
@@ -240,7 +240,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
 
     // Confirmation Page - Scenarios
 
-    Scenario("[F1] ADD-12: The user can change if they are importer or exporter.") {
+    Scenario("[F1] Step-14: The user can change if they are importer or exporter.") {
       Given("the user clicks the link to change if they are importer or exporter")
       importerOrExporterPage.clickLinkToPage()
 
@@ -248,7 +248,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       importerOrExporterPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-12: The user can change the EORI number to add.") {
+    Scenario("[F1] Step-15: The user can change the EORI number to add.") {
       Given("the user returns to the check answers page")
       checkAnswersPage.navigateTo()
 
@@ -259,7 +259,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       eoriNumberPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-12: The user can change the third-party access period") {
+    Scenario("[F1] Step-17: The user can change the third-party access period") {
       Given("the user returns to the check answers page")
       checkAnswersPage.navigateTo()
 
@@ -270,7 +270,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       accessStartPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-12: The user can change the type of data the third party can access") {
+    Scenario("[F1] Step-18: The user can change the type of data the third party can access") {
       Given("the user returns to the check answers page")
       checkAnswersPage.navigateTo()
 
@@ -281,7 +281,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       dataTypesPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-12: The user can change whether they want to give access to all available data") {
+    Scenario("[F1] Step-19: The user can change whether they want to give access to all available data") {
       Given("the user returns to the check answers page")
       checkAnswersPage.navigateTo()
 
@@ -292,7 +292,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       giveAccessToDataPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-12: The user can change the period of data the third party can access") {
+    Scenario("[F1] Step-20: The user can change the period of data the third party can access") {
       Given("the user returns to the check answers page")
       checkAnswersPage.navigateTo()
 
@@ -303,7 +303,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       dataStartPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-12: The user can click to continue to add third party.") {
+    Scenario("[F1] Step-21: The user can click to continue to add third party.") {
       Given("the user returns to the check answers page")
       checkAnswersPage.navigateTo()
 
@@ -317,7 +317,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
 
     // Confirmation Page - Scenarios
 
-    Scenario("[F1] ADD-13: The user can continue from the confirmation page") {
+    Scenario("[F1] Step-22: The user can continue from the confirmation page") {
       Given("the user clicks the link to return to the dashboard")
       dashboardPage.clickLinkToPage()
 
@@ -326,7 +326,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       dashboardPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-3: The user cannot add the same EORI again.") {
+    Scenario("[F1] Step-23: The user cannot add the same EORI again.") {
       Given("the user jumps to the EORI number page.")
       eoriNumberPage.navigateTo()
 
@@ -341,7 +341,7 @@ class ADD_AddThirdPartySpec extends BaseSpec {
       eoriAlreadyAddedPage.assertPageTitle()
     }
 
-    Scenario("[F1] ADD-3-KO: The user can return to the dashboard from the kickout.") {
+    Scenario("[F1] Step-24: The user can return to the dashboard from the kickout.") {
       Given("the user clicks the link to return to the dashboard")
       dashboardPage.clickLinkToPage()
 
