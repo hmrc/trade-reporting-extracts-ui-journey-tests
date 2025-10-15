c77ead356e8901409d8ee07ec158b838693c7a1d
Alex-H-94 <23293049+Alex-H-94@users.noreply.github.com>
2025-10-15 10:47:59 +0100

    Updating Dependencies
    Adding MongoDB client dependency for future work.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,10 +3,11 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "com.vladsch.flexmark" % "flexmark-all"   % "0.64.8" % Test,
-    "org.scalatest"       %% "scalatest"      % "3.2.19" % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner" % "0.49.0" % Test,
-    "com.typesafe"         % "config"         % "1.4.4"  % Test
+    "com.vladsch.flexmark" % "flexmark-all"       % "0.64.8" % Test,
+    "org.scalatest"       %% "scalatest"          % "3.2.19" % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"     % "0.49.0" % Test,
+    "com.typesafe"         % "config"             % "1.4.4"  % Test,
+    "org.mongodb.scala"   %% "mongo-scala-driver" % "5.6.1" cross CrossVersion.for3Use2_13
   )
 
 }
