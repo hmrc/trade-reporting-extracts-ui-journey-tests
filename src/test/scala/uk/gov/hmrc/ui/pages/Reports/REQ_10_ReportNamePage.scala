0b106d102e8f61da542d05bf683f5613a27895dc
Alex-H-94 <23293049+Alex-H-94@users.noreply.github.com>
2025-11-12 12:41:34 +0000

    Removing unneeded import

--- src/test/scala/uk/gov/hmrc/ui/pages/Reports/REQ_10_ReportNamePage.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/Reports/REQ_10_ReportNamePage.scala
@@ -16,8 +16,6 @@
 
 package uk.gov.hmrc.ui.pages
 
-import org.openqa.selenium.By
-
 object REQ_10_ReportNamePage
     extends BasePage("/report-name", "What name will you use to identify this set of reports?") { // Note: "this/set of report[s]" is dynamic to how many report subtypes were selected.
   val inputLimit: Int = 100
