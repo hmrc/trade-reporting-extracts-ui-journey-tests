ece03a43e85e49ba3ad2561a7688ef9b69407801
Alex-H-94 <23293049+Alex-H-94@users.noreply.github.com>
2026-01-08 14:55:57 +0000

    Updated URLs
    Updated page files to use the new static URLs and modified the related tests accordingly.

--- src/test/scala/uk/gov/hmrc/ui/pages/ThirdParty/TPA_4_BusinessRemoveConfirmPage.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/ThirdParty/TPA_4_BusinessRemoveConfirmPage.scala
@@ -16,6 +16,5 @@
 
 package uk.gov.hmrc.ui.pages
 
-// QA Note: The proper URL is "<EORI Number>-access-removed-confirmation" <-- Account for this when writing tests.
 object TPA_4_BusinessRemoveConfirmPage
-    extends BasePage("-access-removed-confirmation", "Your third-party access has been removed") {}
+    extends BasePage("/traderEori-access-removed-confirmation", "Your third-party access has been removed") {}
