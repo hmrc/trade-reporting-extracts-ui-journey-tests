131d1bc81eda4c610009bcc7264940edd8e72d3c
Alex-H-94 <23293049+Alex-H-94@users.noreply.github.com>
2026-01-23 10:22:32 +0000

    Update MongoDB Helper

--- src/test/scala/support/helpers/MongoDBHelper.scala
+++ src/test/scala/support/helpers/MongoDBHelper.scala
@@ -51,9 +51,8 @@ def MongoInsertRecord(doc: MongoDocument): Boolean = {
   // Ready the full document to go into MongoDB, with Third Party data (if any).
   val observable: Observable[InsertOneResult] = collection.insertOne(
     Document(
-      "eori"             -> doc.traderEori,
-      "additionalEmails" -> List(doc.additionalEmail),
-      "authorisedUsers"  -> thirdParty
+      "eori"            -> doc.traderEori,
+      "authorisedUsers" -> thirdParty
     )
   )
 
