f06e477f3df12ef116294d165ef49f7105c00e48
Alex-H-94 <23293049+Alex-H-94@users.noreply.github.com>
2026-09-25 15:13:22 +0100

    LiveHandover: ManageThirdPartySpec : Merged scenarios and removed private values.

--- src/test/scala/uk/gov/hmrc/ui/specs/MTP_ManageThirdPartySpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/MTP_ManageThirdPartySpec.scala
@@ -20,210 +20,163 @@ import support.BaseSpec
 
 class MTP_ManageThirdPartySpec extends BaseSpec {
 
-  private val loginPage                 = AuthLoginStubPage
-  private val dashboardPage             = ACC_1_DashboardPage
-  private val manageThirdPartyPage      = MTP_1_ManageThirdPartyPage
-  private val editThirdPartyDetailsPage = MTP_2_EditThirdPartyDetailsPage
-  private val removeThirdPartyPage      = MTP_3_RemoveThirdPartyPage
-  private val removeConfirmationPage    = MTP_4_RemoveConfirmationPage
+  Feature("The user can EDIT and REMOVE a third party.") {
 
-  private val accessStartPage      = ADD_6_AccessStartPage
-  private val accessEndPage        = ADD_7_AccessEndPage
-  private val dataTypesPage        = ADD_8_DataTypesPage
-  private val giveAccessToDataPage = ADD_9_GiveAccessToDataPage
-  private val dataStartPage        = ADD_10_DataStartPage
-  private val dataEndPage          = ADD_11_DataEndPage
-
-  Feature("[F1] The user can edit a third party.") {
-
-    Scenario(s"[F1] SETUP: Prepare MongoDB with $userThirdPartyEORI already added to $userTraderEori.") {
+    Scenario(s"The user with trader EORI $userThirdPartyEORI can EDIT third party $userTraderEori.") {
       Given("the mongoDB is prepped then a success should be returned.")
       assert(PrepMongoInsertRecord() == true)
-    }
 
-    Scenario("[F1] ACC-1: The user is authenticated.") {
       When(s"the user logs in with EORI $userTraderEori.")
-      loginPage.navigateTo()
-      loginPage.enterRedirectionUrl()
-      loginPage.enterEnrollment(userTraderLogin)
-      loginPage.continue()
+      AuthLoginStubPage.navigateTo()
+      AuthLoginStubPage.enterRedirectionUrl()
+      AuthLoginStubPage.enterEnrollment(userTraderLogin)
+      AuthLoginStubPage.continue()
 
       Then("the user is taken to the dashboard.")
-      dashboardPage.assertUrl()
-      dashboardPage.assertPageTitle()
-    }
+      ACC_1_DashboardPage.assertUrl()
+      ACC_1_DashboardPage.assertPageTitle()
 
-    Scenario(s"[F1] Step-1: The user can nvagiate to the manage third parties page.") {
       When("the user clicks the link on the dashboard")
-      manageThirdPartyPage.clickLinkToPage()
+      MTP_1_ManageThirdPartyPage.clickLinkToPage()
 
       Then("the user is taken to the 'manage' page")
-      manageThirdPartyPage.assertUrl()
-      manageThirdPartyPage.assertPageTitle()
-    }
+      MTP_1_ManageThirdPartyPage.assertUrl()
+      MTP_1_ManageThirdPartyPage.assertPageTitle()
 
-    Scenario(s"[F1] Step-2: The user clicks to edit '$userThirdPartyEORI' details.") {
       Given("the user clicks the 'edit' link.")
-      editThirdPartyDetailsPage.clickLinkToPage(userThirdPartyEORI)
+      MTP_2_EditThirdPartyDetailsPage.clickLinkToPage(userThirdPartyEORI)
 
       Then("the user is taken to the 'edit third party details' page")
-      editThirdPartyDetailsPage.assertUrl(userThirdPartyEORI)
-      editThirdPartyDetailsPage.assertPageTitle()
-    }
+      MTP_2_EditThirdPartyDetailsPage.assertUrl(userThirdPartyEORI)
+      MTP_2_EditThirdPartyDetailsPage.assertPageTitle()
 
-    // Third Party "Access Period"
+      // Third Party "Access Period"
 
-    Scenario(s"[F1] Step-3: The user clicks to change the third-party access period.") {
       Given("the user clicks the 'change' link.")
-      accessStartPage.clickEditLinkToPage(userThirdPartyEORI)
+      ADD_6_AccessStartPage.clickEditLinkToPage(userThirdPartyEORI)
 
       Then("the user is taken to the 'edit access start' page")
-      accessStartPage.assertEditUrl(userThirdPartyEORI)
-      accessStartPage.assertPageTitle()
-    }
+      ADD_6_AccessStartPage.assertEditUrl(userThirdPartyEORI)
+      ADD_6_AccessStartPage.assertPageTitle()
 
-    Scenario(s"[F1] Step-4: The user clicks to continue onto the edit access date end page.") {
       Given("the user clicks to continue.")
-      accessStartPage.continue()
+      ADD_6_AccessStartPage.continue()
 
       Then("the user is taken to the 'edit access end' page")
-      accessEndPage.assertEditUrl(userThirdPartyEORI)
-      accessEndPage.assertPageTitle()
-    }
+      ADD_7_AccessEndPage.assertEditUrl(userThirdPartyEORI)
+      ADD_7_AccessEndPage.assertPageTitle()
 
-    Scenario(s"[F1] Step-5: The user clicks to continue and is returned to '$userThirdPartyEORI' details page.") {
       Given("the user clicks to continue.")
-      accessEndPage.continue()
+      ADD_7_AccessEndPage.continue()
 
       Then("the user is returned to the 'edit third party details' page")
-      editThirdPartyDetailsPage.assertUrl(userThirdPartyEORI)
-      editThirdPartyDetailsPage.assertPageTitle()
-    }
+      MTP_2_EditThirdPartyDetailsPage.assertUrl(userThirdPartyEORI)
+      MTP_2_EditThirdPartyDetailsPage.assertPageTitle()
 
-    // Third Party "Types of Data"
+      // Third Party "Types of Data"
 
-    Scenario(s"[F1] Step-6: The user clicks to change the types of data they have access to.") {
       Given("the user clicks the 'change' link.")
-      dataTypesPage.clickEditLinkToPage(userThirdPartyEORI)
+      ADD_8_DataTypesPage.clickEditLinkToPage(userThirdPartyEORI)
 
       Then("the user is taken to the 'edit types of data' page")
-      dataTypesPage.assertEditUrl(userThirdPartyEORI)
-      dataTypesPage.assertPageTitle()
-    }
+      ADD_8_DataTypesPage.assertEditUrl(userThirdPartyEORI)
+      ADD_8_DataTypesPage.assertPageTitle()
 
-    Scenario(s"[F1] Step-7: The user clicks to continue and is returned to '$userThirdPartyEORI' details page.") {
       Given("the user clicks to continue.")
-      dataTypesPage.continue()
+      ADD_8_DataTypesPage.continue()
 
       Then("the user is returned to the 'edit third party details' page")
-      editThirdPartyDetailsPage.assertUrl(userThirdPartyEORI)
-      editThirdPartyDetailsPage.assertPageTitle()
-    }
+      MTP_2_EditThirdPartyDetailsPage.assertUrl(userThirdPartyEORI)
+      MTP_2_EditThirdPartyDetailsPage.assertPageTitle()
 
-    // Third Party "Data View Period"
+      // Third Party "Data View Period"
 
-    Scenario(s"[F1] Step-8: The user clicks to change the third-party data view period.") {
       Given("the user clicks the 'change' link.")
-      giveAccessToDataPage.clickEditLinkToPage(userThirdPartyEORI)
+      ADD_9_GiveAccessToDataPage.clickEditLinkToPage(userThirdPartyEORI)
 
       Then("the user is taken to the 'all data access' decision page")
-      giveAccessToDataPage.assertEditUrl(userThirdPartyEORI)
-      giveAccessToDataPage.assertPageTitle()
-    }
+      ADD_9_GiveAccessToDataPage.assertEditUrl(userThirdPartyEORI)
+      ADD_9_GiveAccessToDataPage.assertPageTitle()
 
-    Scenario(s"[F1] Step-9: The user selects 'no' and continues onto the edit data view start date page.") {
       Given("the user ensures 'no' is ticked.")
-      giveAccessToDataPage.selectOptionByIndex(1)
+      ADD_9_GiveAccessToDataPage.selectOptionByIndex(1)
 
       And("the user clicks to continue")
-      giveAccessToDataPage.continue()
+      ADD_9_GiveAccessToDataPage.continue()
 
       Then("the user is taken to the 'data view start date' page")
-      dataStartPage.assertEditUrl(userThirdPartyEORI)
-      dataStartPage.assertPageTitle()
-    }
+      ADD_10_DataStartPage.assertEditUrl(userThirdPartyEORI)
+      ADD_10_DataStartPage.assertPageTitle()
 
-    Scenario(s"[F1] Step-10: The user enters a start date and continues onto the edit data view end date page.") {
       Given("the user enters a date up to four years ago from the current date")
-      dataStartPage.clearAndInputKeys(
-        dataStartPage.getDateMinusYears("dd", 4),
-        dataStartPage.inputCustomDay
+      ADD_10_DataStartPage.clearAndInputKeys(
+        ADD_10_DataStartPage.getDateMinusYears("dd", 4),
+        ADD_10_DataStartPage.inputCustomDay
       )
-      dataStartPage.clearAndInputKeys(
-        dataStartPage.getDateMinusYears("MM", 4),
-        dataStartPage.inputCustomMonth
+      ADD_10_DataStartPage.clearAndInputKeys(
+        ADD_10_DataStartPage.getDateMinusYears("MM", 4),
+        ADD_10_DataStartPage.inputCustomMonth
       )
-      dataStartPage.clearAndInputKeys(
-        dataStartPage.getDateMinusYears("yyyy", 4),
-        dataStartPage.inputCustomYear
+      ADD_10_DataStartPage.clearAndInputKeys(
+        ADD_10_DataStartPage.getDateMinusYears("yyyy", 4),
+        ADD_10_DataStartPage.inputCustomYear
       )
 
       And("the user clicks to continue")
-      dataStartPage.continue()
+      ADD_10_DataStartPage.continue()
 
       Then("the user is taken to the 'data view end date' page")
-      dataEndPage.assertEditUrl(userThirdPartyEORI)
-      dataEndPage.assertPageTitle()
-    }
+      ADD_11_DataEndPage.assertEditUrl(userThirdPartyEORI)
+      ADD_11_DataEndPage.assertPageTitle()
 
-    Scenario(s"[F1] Step-11: The user clicks to continue and is returned to '$userThirdPartyEORI' details page.") {
       Given("the user clicks to continue to give unending access.")
-      dataEndPage.continue()
+      ADD_11_DataEndPage.continue()
 
       Then("the user is returned to the 'edit third party details' page")
-      editThirdPartyDetailsPage.assertUrl(userThirdPartyEORI)
-      editThirdPartyDetailsPage.assertPageTitle()
+      MTP_2_EditThirdPartyDetailsPage.assertUrl(userThirdPartyEORI)
+      MTP_2_EditThirdPartyDetailsPage.assertPageTitle()
     }
-  }
 
-  Feature("[F2] The user can remove a third party.") {
-
-    Scenario(s"[F2] Step-1: The user clicks to remove '$userThirdPartyEORI'.") {
+    Scenario(s"The user with trader EORI $userThirdPartyEORI can REMOVE third party $userTraderEori.") {
       Given("the user is on the third party manage page.")
-      manageThirdPartyPage.navigateTo()
+      MTP_1_ManageThirdPartyPage.navigateTo()
 
       When("the user clicks the 'remove' link.")
-      removeThirdPartyPage.clickLinkToPage(userThirdPartyEORI)
+      MTP_3_RemoveThirdPartyPage.clickLinkToPage(userThirdPartyEORI)
 
       Then("the user is taken to the 'remove third party' page")
-      removeThirdPartyPage.assertUrl(userThirdPartyEORI)
-      removeThirdPartyPage.assertPageTitle()
-    }
+      MTP_3_RemoveThirdPartyPage.assertUrl(userThirdPartyEORI)
+      MTP_3_RemoveThirdPartyPage.assertPageTitle()
 
-    Scenario(s"[F2] Step-2: The user clicks 'no' to removing '$userThirdPartyEORI'.") {
       Given("the user clicks 'no' to removing the third party")
-      removeThirdPartyPage.selectYesNo(false)
+      MTP_3_RemoveThirdPartyPage.selectYesNo(false)
 
       When("the user clicks to continue")
-      removeThirdPartyPage.continue()
+      MTP_3_RemoveThirdPartyPage.continue()
 
       Then("the user is taken back to the 'manage third parties' page")
-      manageThirdPartyPage.assertUrl()
-      manageThirdPartyPage.assertPageTitle()
-    }
+      MTP_1_ManageThirdPartyPage.assertUrl()
+      MTP_1_ManageThirdPartyPage.assertPageTitle()
 
-    Scenario(s"[F2] Step-3: The user clicks 'yes' to removing '$userThirdPartyEORI'.") {
       Given("the user returns to the remove page")
-      removeThirdPartyPage.navigateTo(removeThirdPartyPage.pageFullAddress + userThirdPartyEORI)
+      MTP_3_RemoveThirdPartyPage.navigateTo(MTP_3_RemoveThirdPartyPage.pageFullAddress + userThirdPartyEORI)
 
       And("the user clicks 'yes' to removing the third party")
-      removeThirdPartyPage.selectYesNo(true)
+      MTP_3_RemoveThirdPartyPage.selectYesNo(true)
 
       When("the user clicks to continue")
-      removeThirdPartyPage.continue()
+      MTP_3_RemoveThirdPartyPage.continue()
 
       Then("the user is taken to the 'confirmation' page")
-      removeConfirmationPage.assertUrl()
-      removeConfirmationPage.assertPageTitle()
-    }
+      MTP_4_RemoveConfirmationPage.assertUrl()
+      MTP_4_RemoveConfirmationPage.assertPageTitle()
 
-    Scenario(s"[F2] Step-4: The user returns to the dashboard.") {
       When("the user clicks to return to the 'manage' page")
-      manageThirdPartyPage.clickLinkToPage()
+      MTP_1_ManageThirdPartyPage.clickLinkToPage()
 
       Then("the user is taken to the 'manage' page which is now empty again.")
-      manageThirdPartyPage.assertUrl()
-      manageThirdPartyPage.assertPageTitle(manageThirdPartyPage.titleNoThirdPartiesAdded)
+      MTP_1_ManageThirdPartyPage.assertUrl()
+      MTP_1_ManageThirdPartyPage.assertPageTitle(MTP_1_ManageThirdPartyPage.titleNoThirdPartiesAdded)
     }
   }
 }
